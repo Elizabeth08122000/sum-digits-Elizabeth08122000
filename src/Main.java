993f13990f2a1dc69dd40de32db5769393b64309
Liza <bliza0812@gmail.com>
2020-05-19 14:50:35 +0400

    Answer

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt();
+        System.out.println(A%10+(A%100)/10+A/100);
+        in.close();
     }
 }
